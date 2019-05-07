b005df8a8ea8daf47e4bc791fdaa968e8c9e8a02
João Vasconcelos <joao_neto@apple.com>
2019-05-07 18:43:43 -0300

    tic-tac-toe until now

--- src/index.js
+++ src/index.js
@@ -1,12 +1,129 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {  
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square 
+              value = { this.props.squares[i] }
+              onClick = {() => {
+                // This this.props.onClick(i) is the Game.handleClick(i) function
+                this.props.onClick(i)
+              }}
+            />;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squares: new Array(9).fill(null),
+      player: 'X',
+      history: new Array(0),
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history
+    const squares = this.state.squares.slice()
+    const player = this.state.player
+
+    if(calculateWinner(squares) || squares[i]) {
+      return
+    }
+
+    squares[i] = player
+
+    this.setState({
+      squares: squares,
+      player: this.state.player === 'X' ? 'O' : 'X',
+      history: history.concat({
+        squares: squares,
+        player: player,
+      }),
+    })
+  }
+
+  render() {
+    const squares = this.state.squares
+    const player = this.state.player
+    const winner = calculateWinner(squares)
+    const status  = winner ? `Winner: ${winner}` : `Current Player: ${player}`
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          {/* When render Board, we call Board's render() method and add to it props.squares = squares and props.onClick = a function that receives an i and call the handleClick(i) function */}
+          <Board 
+            squares = {squares}
+            onClick = {(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{ status }</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
